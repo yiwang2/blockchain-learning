8d59ebcc2244b6f652c12ccde0788d91a0adc859
yiwang2 <lixinandme@gmail.com>
2020-09-02 13:45:01 +0100

    buy article and event

--- chainskills/trainings/chainlist-upgrade-1/contracts/ChainList.sol
+++ chainskills/trainings/chainlist-upgrade-1/contracts/ChainList.sol
@@ -2,7 +2,8 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract ChainList {
   //state variables
-  address seller;
+  address payable seller;
+  address buyer;
   string name;
   string description;
   uint256 price;
@@ -12,6 +13,13 @@ contract ChainList {
       address indexed _seller,
       string _name,
       uint256 _price);
+
+  event LogBuyArticle (
+    address indexed _seller,
+    address indexed _buyer,
+    string _name,
+    uint256 _price
+  );
 
   //sell an article
   function sellArticle (string memory _name, string memory _description, uint256 _price) public {
@@ -26,9 +34,27 @@ contract ChainList {
   //get article
   function getArticle () public view returns (
     address _seller,
+    address _buyer,
     string memory _name,
     string memory _description,
     uint256 _price) {
-    return (seller, name, description, price);
+    return (seller, _buyer, name, description, price);
+  }
+
+  function buyArticle () payable public{
+    //whether there is an article for sale
+    require (seller != address(0x0));
+    //article not sold
+    require (buyer == address(0x0));
+    //don't buy yourself
+    require (msg.sender != seller);
+    //check corresponds
+    require (msg.value == price);
+
+    buyer = msg.sender;
+    //pay to seller
+    seller.transfer(msg.value);
+    //trigger Events
+    emit LogBuyArticle(seller, buyer, name, price);
   }
 }
